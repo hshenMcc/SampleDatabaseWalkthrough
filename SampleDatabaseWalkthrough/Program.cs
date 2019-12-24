f012a3c744e4fb86131e22e914ff223cd350004e
H Shen <hshen_1998@yahoo.com>
2019-12-23 21:29:53 -0500

    test commit 1

--- SampleDatabaseWalkthrough/Program.cs
+++ SampleDatabaseWalkthrough/Program.cs
@@ -14,6 +14,7 @@ namespace SampleDatabaseWalkthrough
         [STAThread]
         static void Main()
         {
+            Console.WriteLine("hellow");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
